0c813a09853d4bb74f347b5ca3442040186b843e
Drew Smith <drgh056@gmail.com>
2023-03-11 19:24:11 -0600

    Individualized html and CSS. Cleaned js. Updated screen shot.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,39 +11,42 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// create variable for number of user-desired characters in password
+// Create variable for number of user-desired characters in password
 var numOfPassChars;
 
-// create empty array for potential character collection from prompts
+// Create empty array for potential character collection from prompts
 var potentialCharsArray = [];
 
-// create empty array for strings for final confirmation msg
+// Create empty array for strings for final confirmation msg
 var finalConfirmArray = [];
+
+// Create default message for password box
+var defaultBoxMsg = "Your Secure Password Will Appear Here"
 
 // Create objects for each potential character type; bool used in final confirm, query in character selection confirm, and array for password generation
 var uppercase = {
-  charType: "uppercase",
+  charType: "uppercase letters",
   bool: false,
   query: "Would you like your password to contain uppercase letters?",
   array: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
 }
 
 var lowercase = {
-  charType: "lowercase",
+  charType: "lowercase letters",
   bool: false,
   query: "Would you like your password to contain lowercase letters?",
   array: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
 }
 
 var numeric = {
-  charType: "numeric",
+  charType: "numbers",
   bool: false,
   query: "Would you like your password to contain numbers?",
   array:["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
 }
 
 var special = {
-  charType: "special",
+  charType: "special characters",
   bool: false,
   query: "Would you like your password to contain special characters?",
   array: ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\u{005D}", "^", "_", "`", "\u{007B}", "|", "\u{007D}", "~"],
@@ -57,7 +60,7 @@ function getPassLength() {
       return numOfPassChars;
     }
     else if (numOfPassChars === null) {
-      return "Your Secure Password";
+      return defaultBoxMsg;
     }
     else {
       alert("Sorry. The length must be from 8-128 characters. Please try again.")
@@ -65,7 +68,8 @@ function getPassLength() {
     }
 }
 
-// Presents users a confirm for character type. If reply "okay," adds character type into Potential Chars Array, and sets bool for type to true (referenced in final Confirm).
+/* Presents users a confirm for character type. If user reply "okay," adds character type into Potential Chars Array, and sets bool for type to true 
+   (referenced in final Confirm). */
 function addToArrayConfirm(object) {
   var bool = confirm (object.query);
   if (bool === true) {
@@ -75,7 +79,16 @@ function addToArrayConfirm(object) {
   object.bool = bool;
 }
 
-// Generates random password
+// Resets all object bools to false and empty arrays, so user can re-run app without page reload.
+function resetApp() {
+  for (i=0; i < objArray.length; i++) {
+    objArray[i].bool = false;
+  }
+  finalConfirmArray = [];
+  potentialCharsArray = [];
+}
+
+// Main function to generate password; run on button click
 function generatePassword() {
   //Create object array used in for-loop to determine what characters will be in password
   objArray = [uppercase, lowercase, numeric, special]
@@ -83,7 +96,7 @@ function generatePassword() {
   getPassLength();
 
   // If user presses cancel on character length entry, ends script.
-  if (numOfPassChars === null) return "Your Secure Password";
+  if (numOfPassChars === null) return defaultBoxMsg;
 
   // Calls function loop to add user selected char types to Potential Chars Array.
   for (i=0; i < objArray.length; i++) {
@@ -93,7 +106,7 @@ function generatePassword() {
   // If user responds "cancel" to all confirms, then cancels function.
   if (potentialCharsArray.length <= 0) {
     confirm("Sorry. You must select at least one character type. Please try again.")
-    return "Your Secure Password"; 
+    return defaultBoxMsg; 
   }
   // Final confirm message before password creation. "Okay" creates password; "Cancel" ends function.
   else {
@@ -102,13 +115,9 @@ function generatePassword() {
   
     // If user presses "Cancel," reset all bools and arrays.
     if (finalOkay === false) {
-      for (i=0; i < objArray.length; i++) {
-        objArray[i].bool = false;
-      }
-      finalConfirmArray = [];
-      potentialCharsArray = [];
+      resetApp();
       alert("Sorry. Please try again.");
-      return "Your Secure Password";
+      return defaultBoxMsg;
     } 
   }
 
@@ -123,11 +132,7 @@ function generatePassword() {
   }
 
   // Reset all bools and arrays, so function can be run again without page reload. 
-  for (i=0; i < objArray.length; i++) {
-    objArray[i].bool = false;
-  }
-  finalConfirmArray = []; 
-  potentialCharsArray = [];
+resetApp();
 
   return randomPassword;
 }
